a97bb5ec17e59dfeca456381cbc58e4a0c3c6062
utsavdotdev <95236817+utsavdotdev@users.noreply.github.com>
2026-07-07 13:30:48 +0545

    chore: remove the dev configuration

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,5 @@
 import { Redirect } from 'expo-router';
-import { getInitialHref } from '../src/config/devMode';
 
 export default function Index() {
-  return <Redirect href={getInitialHref() as any} />;
+  return <Redirect href={'/(auth)/loading' as any} />;
 }
